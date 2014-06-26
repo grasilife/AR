918b3d57fc103f20f78030a656f83d5e44758a62
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2014-06-26 17:01:57 +0400

    Test Activity to check fbreader-poweramp communication

--- ARPlugin/src/org/jetbrains/hackathon/ar/MainArActivity.java
+++ ARPlugin/src/org/jetbrains/hackathon/ar/MainArActivity.java
@@ -1,15 +1,54 @@
 package org.jetbrains.hackathon.ar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.maxmpz.poweramp.player.PowerampAPI;
+import org.geometerplus.android.fbreader.api.ApiClientImplementation;
+import org.geometerplus.android.fbreader.api.ApiException;
+import org.geometerplus.android.fbreader.api.TextPosition;
 
-public class MainArActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class MainArActivity extends Activity implements ApiClientImplementation.ConnectionListener {
+    private ApiClientImplementation myApi;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        myApi = new ApiClientImplementation(this, this);
+    }
+
+    @Override
+    protected void onResume() {
+        myApi.connect();
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        myApi.disconnect();
+        super.onDestroy();
+    }
+
+    public void onConnected() {
+        try {
+            myApi.setPageStart(new TextPosition(55, 0, 0));
+            startService(new Intent(PowerampAPI.ACTION_API_COMMAND).putExtra(PowerampAPI.COMMAND, PowerampAPI.Commands.TOGGLE_PLAY_PAUSE));
+        } catch (ApiException e) {
+            showErrorMessage(e.getMessage(), false);
+        }
+    }
+
+    private void showErrorMessage(final CharSequence text, final boolean fatal) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                if (fatal) {
+                    setTitle("FATAL ERROR");
+                }
+                Toast.makeText(MainArActivity.this, text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
