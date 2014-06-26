a907aa6ebee10ee040b56f5f339676e62d49a898
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2014-06-26 13:02:49 +0400

    Android plugin: initial commit

---
+++ ARPlugin/src/org/jetbrains/hackathon/ar/MainArActivity.java
@@ -0,0 +1,15 @@
+package org.jetbrains.hackathon.ar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainArActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
